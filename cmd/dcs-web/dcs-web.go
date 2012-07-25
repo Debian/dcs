8144a3cba3a5c9531ac2ff500762476051c6b299
Michael Stapelberg <michael@stapelberg.de>
2012-07-26 01:31:41 +0200

    dcs-web: ask the source-backend for precise matches

--- cmd/dcs-web/dcs-web.go
+++ cmd/dcs-web/dcs-web.go
@@ -9,10 +9,33 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"sort"
 	"strings"
 )
 
 var indexBackends *string = flag.String("index_backends", "localhost:28081", "Index backends")
+
+type Match struct {
+	Path    string
+	Line    int
+	Ranking int
+	Context string
+}
+
+// This type implements sort.Interface so that we can sort it by rank.
+type SearchResults []Match
+
+func (s SearchResults) Len() int {
+	return len(s)
+}
+
+func (s SearchResults) Less(i, j int) bool {
+	return s[i].Ranking < s[j].Ranking
+}
+
+func (s SearchResults) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `<html><form action="/search" method="get"><input type="text" name="q"><input type="submit">`)
@@ -22,7 +45,7 @@ func sendIndexQuery(query url.URL, backend string, indexResults chan string, don
 	query.Scheme = "http"
 	query.Host = backend
 	query.Path = "/index"
-	log.Printf("asking " + query.String())
+	log.Printf("asking %s\n", query.String())
 	resp, err := http.Get(query.String())
 	if err != nil {
 		done <- true
@@ -52,6 +75,48 @@ func sendIndexQuery(query url.URL, backend string, indexResults chan string, don
 	done <- true
 }
 
+// TODO: refactor this with sendIndexQuery.
+func sendSourceQuery(query url.URL, filename string, matches chan Match, done chan bool) {
+	query.Scheme = "http"
+	// TODO: make this configurable
+	query.Host = "localhost:28082"
+	query.Path = "/source"
+	q := query.Query()
+	q.Set("filename", filename)
+	query.RawQuery = q.Encode()
+	log.Printf("(source) asking %s\n", query.String())
+	resp, err := http.Get(query.String())
+	if err != nil {
+		done <- true
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		done <- true
+		return
+	}
+
+	var beMatches []Match
+	if err := json.Unmarshal(body, &beMatches); err != nil {
+		log.Printf("Invalid result from backend (source)")
+		done <- true
+		return
+	}
+
+	for _, match := range beMatches {
+		matches <- match
+	}
+
+	done <- true
+}
+
+func (m *Match) Rank() {
+	// TODO: compute a rank
+	m.Ranking = 3
+}
+
 func Search(w http.ResponseWriter, r *http.Request) {
 	query := r.URL
 	log.Printf(`Search query for "` + query.String() + `"`)
@@ -69,12 +134,13 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		go sendIndexQuery(*query, backend, indexResults, done)
 	}
 
+	var files []string
 	fmt.Fprintf(w, `<html>Results:<ul>`)
 	for i := 0; i < len(backends); {
 		select {
 		case result := <-indexResults:
 			fmt.Printf("Got a result: %s\n", result)
-			fmt.Fprintf(w, `<li>%s</li>`, result)
+			files = append(files, result)
 			// TODO: we need a sorted data structure in which we can insert the filename
 
 		case <-done:
@@ -82,8 +148,34 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// TODO: Essentially, this follows the MapReduce pattern. Our input is the
+	// filename list, we map that onto matches in the first step (remote), then
+	// we map rankings onto it and then we need it sorted. Maybe there is some
+	// Go package already which can help us here?
+
 	// TODO: We got all the results, now rank them, keep the top 10, then query
-	// all the source code files.
+	// all the source code files. The problem here is that, depending on how we
+	// do the ranking, we need all the precise matches first :-/.
+	matches := make(chan Match)
+	for _, filename := range files {
+		go sendSourceQuery(*query, filename, matches, done)
+	}
+
+	var results SearchResults
+	for i := 0; i < len(files); {
+		select {
+		case match := <-matches:
+			match.Rank()
+			results = append(results, match)
+		case <-done:
+			i++
+		}
+	}
+
+	sort.Sort(results)
+	for _, result := range results {
+		fmt.Fprintf(w, `<li><code>%s</code>:%d<br><code>%s</code></li>`, result.Path, result.Line, result.Context)
+	}
 
 	fmt.Fprintf(w, `</ul>`)
 }
