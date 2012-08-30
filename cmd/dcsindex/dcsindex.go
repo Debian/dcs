283bd4e80dd43c2c6207dfa80a4d66a5ef154094
Michael Stapelberg <michael@stapelberg.de>
2012-08-30 14:15:25 +0200

    dcsindex: update mirror path, document runtime, more ignored files

--- cmd/dcsindex/dcsindex.go
+++ cmd/dcsindex/dcsindex.go
@@ -3,6 +3,9 @@
 // Indexing tool for Debian Code Search. The tool scans through an unpacked
 // source package mirror and adds relevant files to the regular expression
 // trigram index.
+//
+// One run takes about 3 hours when using a slow USB disk as TMPDIR and storing
+// the index output on a SSD.
 package main
 
 import (
@@ -10,6 +13,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 )
@@ -17,8 +21,8 @@ import (
 var numShards = flag.Int("shards", 1,
 	"Number of index shards (the index will be split into 'shard' different files)")
 var mirrorPath = flag.String("mirrorpath",
-	"/media/sdg/debian-source-mirror/unpacked/",
-	"Path to an unpacked Debian source package mirror")
+	"/dcs-ssd/",
+	"Path to an Debian source package mirror with an /unpacked directory inside")
 
 // Returns true when the file matches .[0-9]$ (cheaper than a regular
 // expression).
@@ -37,7 +41,7 @@ func main() {
 	// regular expression to the index.
 	ix := make([]*index.IndexWriter, *numShards)
 	for i := 0; i < *numShards; i++ {
-		path := fmt.Sprintf("/media/sdg/debian-source-mirror/index.%d.idx", i)
+		path := fmt.Sprintf("%s/index.%d.idx", *mirrorPath, i)
 		ix[i] = index.Create(path)
 		ix[i].Verbose = true
 	}
@@ -46,31 +50,50 @@ func main() {
 	//ix.AddPaths([]string{ mirrorPath })
 
 	cnt := 0
-	filepath.Walk(*mirrorPath, func(path string, info os.FileInfo, err error) error {
-		if _, filename := filepath.Split(path); filename != "" {
-			// Skip quilt’s .pc directories
-			if filename == ".pc" && info.IsDir() {
-				return filepath.SkipDir
-			}
+	filepath.Walk(path.Join(*mirrorPath, "unpacked"),
+		func(path string, info os.FileInfo, err error) error {
+			if _, filename := filepath.Split(path); filename != "" {
+				// Skip quilt’s .pc directories and "po" directories (localization)
+				if info.IsDir() &&
+					(filename == ".pc" ||
+						filename == "po") {
+					return filepath.SkipDir
+				}
 
-			// Skip documentation, configuration files and patches.
-			if filename == "README" ||
-				filename == "NEWS" ||
-				strings.HasSuffix(filename, ".man") ||
-				strings.HasSuffix(filename, ".xml") ||
-				strings.HasSuffix(filename, ".html") ||
-				strings.HasSuffix(filename, ".pod") ||
-				strings.HasSuffix(filename, ".patch") ||
-				hasManpageSuffix(filename) {
-				return nil
+				// NB: we don’t skip "configure" since that might be a custom shell-script
+				// Skip documentation, configuration files and patches.
+				if filename == "NEWS" ||
+					filename == "COPYING" ||
+					filename == "LICENSE" ||
+					strings.HasSuffix(filename, ".conf") ||
+					// spell checking dictionaries
+					strings.HasSuffix(filename, ".dic") ||
+					strings.HasSuffix(filename, ".cfg") ||
+					strings.HasSuffix(filename, ".man") ||
+					strings.HasSuffix(filename, ".xml") ||
+					strings.HasSuffix(filename, ".xsl") ||
+					strings.HasSuffix(filename, ".html") ||
+					strings.HasSuffix(filename, ".sgml") ||
+					strings.HasSuffix(filename, ".pod") ||
+					strings.HasSuffix(filename, ".po") ||
+					strings.HasSuffix(filename, ".patch") ||
+					strings.HasSuffix(filename, ".txt") ||
+					strings.HasSuffix(filename, ".tex") ||
+					strings.HasSuffix(filename, ".rtf") ||
+					strings.HasSuffix(filename, ".docbook") ||
+					strings.HasSuffix(filename, ".symbols") ||
+					strings.HasPrefix(strings.ToLower(filename), "changelog") ||
+					strings.HasPrefix(strings.ToLower(filename), "readme") ||
+					hasManpageSuffix(filename) {
+					return nil
+				}
 			}
-		}
-		if info != nil && info.Mode()&os.ModeType == 0 {
-			ix[cnt%*numShards].AddFile(path)
-			cnt++
-		}
-		return nil
-	})
+			if info != nil && info.Mode()&os.ModeType == 0 {
+				ix[cnt%*numShards].AddFile(path)
+				cnt++
+			}
+			return nil
+		})
 	for i := 0; i < *numShards; i++ {
 		ix[i].Flush()
 	}
