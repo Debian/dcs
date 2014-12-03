10f28d3b6dbf12a94144fecbfa7f8c2187926e2c
Michael Stapelberg <michael@stapelberg.de>
2014-12-03 16:20:27 +0100

    implement server-rendered (non-js) per-package results view

--- cmd/dcs-web/serverrendered.go
+++ cmd/dcs-web/serverrendered.go
@@ -13,6 +13,7 @@ import (
 	"html/template"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"os"
@@ -20,6 +21,19 @@ import (
 	"strconv"
 	"strings"
 )
+
+// XXX: Using a dcsregexp.Match anonymous struct member doesn’t work,
+// because we need to assign to the members to get the data from Result
+// over into halfRenderedResult.
+type halfRenderedResult struct {
+	Path          string
+	Line          int
+	PathRank      float32
+	Ranking       float32
+	SourcePackage string
+	RelativePath  string
+	Context       template.HTML
+}
 
 func maybeAppendContext(context []string, line string) []string {
 	if strings.TrimSpace(line) != "" {
@@ -47,10 +61,6 @@ func splitPath(path string) (sourcePackage string, relativePath string) {
 // NB: Updates to this function must also be performed in static/instant.js.
 func updatePagination(currentpage int, resultpages int, baseurl string) string {
 	result := `<strong>Pages:</strong> `
-	if currentpage > 0 {
-		result = result + `<a href="` + baseurl + `&page=0">1</a> `
-		result = result + `<span>&lt;</span> `
-	}
 	start := currentpage - 5
 	if currentpage > 0 && 1 > start {
 		start = 1
@@ -66,6 +76,15 @@ func updatePagination(currentpage int, resultpages int, baseurl string) string {
 		end = 10
 	}
 
+	if currentpage > 0 {
+		result = result + `<a href="` + baseurl + `&page=` + strconv.Itoa(currentpage-1) + `" rel="prev">&lt;</a> `
+		result = result + `<a href="` + baseurl + `&page=0">1</a> `
+	}
+
+	if start > 1 {
+		result = result + `… `
+	}
+
 	for i := start; i < end; i++ {
 		result = result + `<a style="`
 		if i == currentpage {
@@ -74,20 +93,131 @@ func updatePagination(currentpage int, resultpages int, baseurl string) string {
 		result = result + `" href="` + baseurl + `&page=` + strconv.Itoa(i) + `">` + strconv.Itoa(i+1) + `</a> `
 	}
 
-	if currentpage < (resultpages - 1) {
-		result = result + `<span>&gt;</span> `
+	if end < (resultpages - 1) {
+		result = result + `… `
 	}
 
 	if end < resultpages {
-		result = result + `… <a href="` + baseurl + `&page=` + strconv.Itoa(resultpages-1) + `">` + strconv.Itoa(resultpages) + `</a>`
+		result = result + `<a href="` + baseurl + `&page=` + strconv.Itoa(resultpages-1) + `">` + strconv.Itoa(resultpages) + `</a>`
+	}
+
+	if currentpage < (resultpages - 1) {
+		result = result + ` <a href="` + baseurl + `&page=` + strconv.Itoa(currentpage+1) + `" rel="next">&gt;</a> `
 	}
 
 	return result
 }
 
+func readPackagesFile(queryid string) ([]string, error) {
+	packagesFile, err := os.Open(filepath.Join(*queryResultsPath, queryid, "packages.json"))
+	if err != nil {
+		return []string{}, fmt.Errorf("Could not open packages file on disk: %v", err)
+	}
+	defer packagesFile.Close()
+
+	var packages struct {
+		Packages []string
+	}
+
+	if err := json.NewDecoder(packagesFile).Decode(&packages); err != nil {
+		return []string{}, fmt.Errorf("Could not parse packages from disk: %v", err)
+	}
+
+	end := 100
+	if end > len(packages.Packages) {
+		end = len(packages.Packages) - 1
+	}
+	return packages.Packages[:end], nil
+}
+
+func renderPerPackage(w http.ResponseWriter, r *http.Request, queryid string, page int) {
+	dir := filepath.Join(*queryResultsPath, queryid)
+	name := filepath.Join(dir, fmt.Sprintf("perpackage_2_page_%d.json", page))
+	resultsFile, err := os.Open(name)
+	if err != nil {
+		http.Error(w,
+			fmt.Sprintf("Could not open results file on disk: %v", err),
+			http.StatusInternalServerError)
+		return
+	}
+	defer resultsFile.Close()
+
+	type perPackageResults struct {
+		Package    string
+		RawResults []Result `json:"Results"`
+		Results    []halfRenderedResult
+	}
+
+	var results []perPackageResults
+	if err := json.NewDecoder(resultsFile).Decode(&results); err != nil {
+		http.Error(w,
+			fmt.Sprintf("Could not parse results from disk: %v", err),
+			http.StatusInternalServerError)
+		return
+	}
+
+	for idx, pp := range results {
+		halfrendered := make([]halfRenderedResult, len(pp.RawResults))
+		for idx, result := range pp.RawResults {
+			var context []string
+			context = maybeAppendContext(context, result.Ctxp2)
+			context = maybeAppendContext(context, result.Ctxp1)
+			context = append(context, "<strong>"+result.Context+"</strong>")
+			context = maybeAppendContext(context, result.Ctxn1)
+			context = maybeAppendContext(context, result.Ctxn2)
+
+			sourcePackage, relativePath := splitPath(result.Path)
+
+			halfrendered[idx] = halfRenderedResult{
+				Path:          result.Path,
+				Line:          result.Line,
+				PathRank:      result.PathRank,
+				Ranking:       result.Ranking,
+				SourcePackage: sourcePackage,
+				RelativePath:  relativePath,
+				Context:       template.HTML(strings.Join(context, "<br>")),
+			}
+		}
+		results[idx] = perPackageResults{
+			Package: pp.Package,
+			Results: halfrendered,
+		}
+	}
+
+	packages, err := readPackagesFile(queryid)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	basequery := r.URL.Query()
+	basequery.Del("page")
+	baseurl := r.URL
+	baseurl.RawQuery = basequery.Encode()
+	pages := int(math.Ceil(float64(len(state[queryid].allPackagesSorted)) / float64(packagesPerPage)))
+	pagination := updatePagination(page, pages, baseurl.String())
+
+	basequery.Del("perpkg")
+	basequery.Del("q")
+	baseurl.RawQuery = basequery.Encode()
+	filterurl := baseurl.String()
+
+	if err := common.Templates.ExecuteTemplate(w, "perpackage-results.html", map[string]interface{}{
+		"results":    results,
+		"filterurl":  filterurl,
+		"packages":   packages,
+		"pagination": template.HTML(pagination),
+		"q":          r.Form.Get("q"),
+		"version":    common.Version,
+	}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 // q= search term
 // page= page number
-// TODO: perpkg= per-package grouping
+// perpkg= per-package grouping
 func Search(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Could not parse form data", http.StatusInternalServerError)
@@ -136,6 +266,11 @@ func Search(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("[%s] server-rendering page %d\n", queryid, page)
 
+	if r.Form.Get("perpkg") == "1" {
+		renderPerPackage(w, r, queryid, page)
+		return
+	}
+
 	dir := filepath.Join(*queryResultsPath, queryid)
 	name := filepath.Join(dir, fmt.Sprintf("page_%d.json", page))
 	resultsFile, err := os.Open(name)
@@ -155,20 +290,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// XXX: Using a dcsregexp.Match anonymous struct member doesn’t work,
-	// because we need to assign to the members to get the data from Result
-	// over into HalfRenderedResult.
-	type HalfRenderedResult struct {
-		Path          string
-		Line          int
-		PathRank      float32
-		Ranking       float32
-		SourcePackage string
-		RelativePath  string
-		Context       template.HTML
-	}
-
-	halfrendered := make([]HalfRenderedResult, len(results))
+	halfrendered := make([]halfRenderedResult, len(results))
 	for idx, result := range results {
 		var context []string
 		context = maybeAppendContext(context, result.Ctxp2)
@@ -179,7 +301,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 
 		sourcePackage, relativePath := splitPath(result.Path)
 
-		halfrendered[idx] = HalfRenderedResult{
+		halfrendered[idx] = halfRenderedResult{
 			Path:          result.Path,
 			Line:          result.Line,
 			PathRank:      result.PathRank,
@@ -190,23 +312,9 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	packagesFile, err := os.Open(filepath.Join(dir, "packages.json"))
+	packages, err := readPackagesFile(queryid)
 	if err != nil {
-		http.Error(w,
-			fmt.Sprintf("Could not open packages file on disk: %v", err),
-			http.StatusInternalServerError)
-		return
-	}
-	defer packagesFile.Close()
-
-	var packages struct {
-		Packages []string
-	}
-
-	if err := json.NewDecoder(packagesFile).Decode(&packages); err != nil {
-		http.Error(w,
-			fmt.Sprintf("Could not parse packages from disk: %v", err),
-			http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -216,9 +324,20 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	baseurl.RawQuery = basequery.Encode()
 	pagination := updatePagination(page, state[queryid].resultPages, baseurl.String())
 
+	basequery.Set("perpkg", "1")
+	baseurl.RawQuery = basequery.Encode()
+	perpkgurl := baseurl.String()
+
+	basequery.Del("perpkg")
+	basequery.Del("q")
+	baseurl.RawQuery = basequery.Encode()
+	filterurl := baseurl.String()
+
 	if err := common.Templates.ExecuteTemplate(w, "results.html", map[string]interface{}{
+		"perpkgurl":  perpkgurl,
+		"filterurl":  filterurl,
 		"results":    halfrendered,
-		"packages":   packages.Packages,
+		"packages":   packages,
 		"pagination": template.HTML(pagination),
 		"q":          r.Form.Get("q"),
 		"version":    common.Version,
