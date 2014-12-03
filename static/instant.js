b729442e5096e062451308af66e1578ab910d403
Michael Stapelberg <michael@stapelberg.de>
2014-12-03 12:33:49 +0100

    add error message for “failed” type errors

--- static/instant.js
+++ static/instant.js
@@ -473,6 +473,8 @@ connection.onmessage = function(e) {
             error(false, true, msg.ErrorType, "The results may be incomplete, not all Debian Code Search servers are okay right now.");
         } else if (msg.ErrorType == "cancelled") {
             error(false, true, msg.ErrorType, "This query has been cancelled by the server administrator (to preserve overall service health).");
+        } else if (msg.ErrorType == "failed") {
+            error(false, true, msg.ErrorType, "This query failed due to an unexpected internal server error.");
         } else {
             error(msg.ErrorType);
         }
