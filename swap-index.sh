73095c2c5e73dcda2ca297104a5f99305ec28e0e
Michael Stapelberg <michael@stapelberg.de>
2014-12-09 08:35:20 +0100

    delete old index update scripts
    Creating a new index and telling the index-backend to load it are now
    done by dcs-package-importer.

--- swap-index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-mv /dcs/index.*.idx /dcs/OLD/
-mv /dcs/NEW/index.*.idx /dcs/
-mv /dcs/unpacked /dcs/OLD/unpacked
-mv /dcs/unpacked-new /dcs/unpacked
-for i in $(seq 0 5); do
-	systemctl restart dcs-index-backend@$i.service
-done
