4baa94c33679b4fda61d8a48c1db404132dcc654
Michael Stapelberg <michael@stapelberg.de>
2014-12-13 22:52:58 +0100

    index-backend: lock mutex when swapping
    This was a problem for the first time today in production, so let’s fix
    it for good :).

--- cmd/dcs-index-backend/index-backend.go
+++ cmd/dcs-index-backend/index-backend.go
@@ -13,16 +13,18 @@ import (
 	"os"
 	"path/filepath"
 	"runtime/pprof"
+	"sync"
 	"time"
 )
-
-var ix *index.Index
 
 var (
 	listenAddress = flag.String("listen_address", ":28081", "listen address ([host]:port)")
 	indexPath     = flag.String("index_path", "", "path to the index shard to serve, e.g. /dcs-ssd/index.0.idx")
 	cpuProfile    = flag.String("cpuprofile", "", "write cpu profile to this file")
-	id            string
+
+	id      string
+	ix      *index.Index
+	ixMutex sync.Mutex
 )
 
 // Handles requests to /index by compiling the q= parameter into a regular
@@ -51,6 +53,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	query := index.RegexpQuery(re.Syntax)
 	log.Printf("[%s] query: text = %s, regexp = %s\n", id, textQuery, query)
 	t0 := time.Now()
+	ixMutex.Lock()
 	post := ix.PostingQuery(query)
 	t1 := time.Now()
 	fmt.Printf("[%s] postingquery done in %v, %d results\n", id, t1.Sub(t0), len(post))
@@ -58,6 +61,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	for idx, fileid := range post {
 		files[idx] = ix.Name(fileid)
 	}
+	ixMutex.Unlock()
 	t2 := time.Now()
 	fmt.Printf("[%s] filenames collected in %v\n", id, t2.Sub(t1))
 	if err := json.NewEncoder(w).Encode(files); err != nil {
@@ -68,9 +72,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[%s] written in %v\n", id, t3.Sub(t2))
 }
 
-// XXX: theoretically, we need to make sure no Index() call is currently in
-// progress since they may return incorrect result if “ix” suddenly points to a
-// different index.
 func Replace(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	newShard := r.Form.Get("shard")
@@ -92,7 +93,9 @@ func Replace(w http.ResponseWriter, r *http.Request) {
 			// this directory, so let’s load this shard.
 			oldIndex := ix
 			log.Printf("Trying to load %q\n", newShard)
+			ixMutex.Lock()
 			ix = index.Open(newShard)
+			ixMutex.Unlock()
 			// Overwrite the old full shard with the new one. This is necessary
 			// so that the state is persistent across restarts and has the nice
 			// side-effect of cleaning up the old full shard.
