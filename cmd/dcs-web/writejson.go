67ef934535b9a08a0664c49c52e8f399807cb63e
Michael Stapelberg <michael@stapelberg.de>
2016-03-26 19:47:29 +0100

    Add forgotten writejson.go

---
+++ cmd/dcs-web/writejson.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+
+	pb "github.com/Debian/dcs/proto"
+)
+
+// WriteMatchJSON was generated when we were still using capnproto.
+// TODO: investigate whether any further performance tuning with regards to
+// generating JSON makes sense.
+func WriteMatchJSON(match *pb.Match, w io.Writer) error {
+	b := bufio.NewWriter(w)
+	var err error
+	var buf []byte
+	_ = buf
+	err = b.WriteByte('{')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"path\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Path
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"line\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Line
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"ctxp2\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Ctxp2
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"ctxp1\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Ctxp1
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"context\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Context
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"ctxn1\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Ctxn1
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"ctxn2\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Ctxn2
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"pathrank\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Pathrank
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"ranking\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Ranking
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte(',')
+	if err != nil {
+		return err
+	}
+	_, err = b.WriteString("\"package\":")
+	if err != nil {
+		return err
+	}
+	{
+		s := match.Package
+		buf, err = json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		_, err = b.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	err = b.WriteByte('}')
+	if err != nil {
+		return err
+	}
+	err = b.Flush()
+	return err
+}
