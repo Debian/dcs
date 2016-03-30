Debian/dcs

commit e0c55fd66c79cc622c0efe710e808474c21da1eb

Strip leading/trailing filters, but preserve entire search term
Currently, extra whitespace around filters can cause a search to
incorrectly fail.  Whitespace within a search string is relevant, and
should be preserved, but whitespace between filters and the search
string should not be relevant.

That is " package:foo   bar    baz" is a search for "bar    baz" in
package foo, not a search for "  bar    baz".

This commit changes how filters are found by only looking for them at
the start/end of the search string.  Any found filters are stripped and
the remainder of the search string is used for the final query.

Signed-off-by: James McCoy <jamessan@debian.org>

cmd/dcs-web/search/query-rewrite.go
@@ -2,43 +2,57 @@
 package search
 
 import (
-	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 )
+
+var (
+	start = regexp.MustCompile(`(?i)^\s*(-?(?:filetype|package|pkg|path|file)):(\S+)\s+`)
+	end   = regexp.MustCompile(`(?i)\s+(-?(?:filetype|package|pkg|path|file)):(\S+)\s*$`)
+)
+
+func rewriteFilters(query url.Values, filtersRe *regexp.Regexp) url.Values {
+	qstr := query.Get("q")
+	// Using a regexp so the start/end indices of each match are easily
+	// available for subsequent slicing of the query string.  The performance
+	// (regexp @ 0.011 ms vs. string @ 0.004 ms) is minimal enough that the
+	// extra hit for regexp is acceptable, given the simpler code.
+	matches := filtersRe.FindStringSubmatch(qstr)
+	for matches != nil {
+		// matches is [entire_match, filter_name, filter_value]
+		filter := strings.ToLower(matches[1])
+		value := matches[2]
+
+		if filter == "-file" {
+			filter = "npath"
+		} else if strings.HasPrefix(filter, "-") {
+			filter = "n" + filter[1:]
+		}
+		if strings.HasSuffix(filter, "filetype") {
+			value = strings.ToLower(value)
+		}
+		query.Add(filter, value)
+
+		if filtersRe == start {
+			qstr = strings.TrimPrefix(qstr, matches[0])
+		} else {
+			qstr = strings.TrimSuffix(qstr, matches[0])
+		}
+		matches = filtersRe.FindStringSubmatch(qstr)
+	}
+
+	query.Set("q", qstr)
+	return query
+}
 
 // Parses the querystring (q= parameter) and moves special tokens such as
 // "lang:c" from the querystring into separate arguments.
 func RewriteQuery(u url.URL) url.URL {
 	// query is a copy which we will modify using Set() and use in the result
-	query := u.Query()
+	query := rewriteFilters(u.Query(), start)
+	query = rewriteFilters(query, end)
 
-	querystr := query.Get("q")
-	queryWords := []string{}
-	for _, word := range strings.Split(querystr, " ") {
-		fmt.Printf("word = %v\n", word)
-		lower := strings.ToLower(word)
-		if strings.HasPrefix(lower, "filetype:") {
-			query.Add("filetype", strings.ToLower(word[len("filetype:"):]))
-		} else if strings.HasPrefix(lower, "-filetype:") {
-			query.Add("nfiletype", strings.ToLower(word[len("-filetype:"):]))
-		} else if strings.HasPrefix(lower, "package:") {
-			query.Set("package", word[len("package:"):])
-		} else if strings.HasPrefix(lower, "pkg:") {
-			query.Set("package", word[len("pkg:"):])
-		} else if strings.HasPrefix(lower, "-package:") {
-			query.Add("npackage", word[len("-package:"):])
-		} else if strings.HasPrefix(lower, "-pkg:") {
-			query.Add("npackage", word[len("-pkg:"):])
-		} else if strings.HasPrefix(lower, "path:") || strings.HasPrefix(lower, "file:") {
-			query.Add("path", word[len("path:"):])
-		} else if strings.HasPrefix(lower, "-path:") || strings.HasPrefix(lower, "-file:") {
-			query.Add("npath", word[len("-path:"):])
-		} else {
-			queryWords = append(queryWords, word)
-		}
-	}
-	query.Set("q", strings.Join(queryWords, " "))
 	u.RawQuery = query.Encode()
 
 	return u

cmd/dcs-web/search/query-rewrite_test.go
@@ -23,6 +23,20 @@ func TestRewriteQuery(t *testing.T) {
 		t.Fatalf("Expected search query %s, got %s", "searchterm", querystr)
 	}
 
+	// Verify that the amount of whitespace between filters and search terms aren't relevant
+	rewritten = rewrite(t, "/search?q=package:foo++searchterm")
+	querystr = rewritten.Query().Get("q")
+	if querystr != "searchterm" {
+		t.Fatalf("Expected search query '%s', got '%s'", "searchterm", querystr)
+	}
+
+	// Verify that the amount of in a search term is relevant
+	rewritten = rewrite(t, "/search?q=package:foo+search++term")
+	querystr = rewritten.Query().Get("q")
+	if querystr != "search  term" {
+		t.Fatalf("Expected search query '%s', got '%s'", "search term", querystr)
+	}
+
 	// Verify that the filetype: keyword is properly moved
 	rewritten = rewrite(t, "/search?q=searchterm+filetype%3Ac")
 	querystr = rewritten.Query().Get("q")
@@ -65,6 +79,17 @@ func TestRewriteQuery(t *testing.T) {
 	pkg = rewritten.Query().Get("npackage")
 	if pkg != "i3-WM" {
 		t.Fatalf("Expected npackage %s, got %s", "i3-WM", pkg)
+	}
+
+	// Verify that -file is translated to npath
+	rewritten = rewrite(t, "/search?q=searchterm+-file:foo")
+	querystr = rewritten.Query().Get("q")
+	if querystr != "searchterm" {
+		t.Fatalf("Expected search query %s, got %s", "searchterm", querystr)
+	}
+	file := rewritten.Query().Get("npath")
+	if file != "foo" {
+		t.Fatalf("Expected npath %s, got %s", "foo", file)
 	}
 
 	// Verify that the multiple keywords work as expected

commit 776566263ac41dce8df30b0c1bc370806a23d18e

query-rewrite: Re-add support for the pkg keyword
Regression introduced in e0c55fd66c79cc622c0efe710e808474c21da1eb.

Closes #65
Signed-off-by: James McCoy <jamessan@debian.org>

cmd/dcs-web/search/query-rewrite.go
@@ -24,6 +24,7 @@ func rewriteFilters(query url.Values, filtersRe *regexp.Regexp) url.Values {
 		filter := strings.ToLower(matches[1])
 		value := matches[2]
 
+		filter = strings.Replace(filter, "pkg", "package", 1)
 		if filter == "-file" {
 			filter = "npath"
 		} else if strings.HasPrefix(filter, "-") {

cmd/dcs-web/search/query-rewrite_test.go
@@ -92,6 +92,28 @@ func TestRewriteQuery(t *testing.T) {
 		t.Fatalf("Expected npackage %q, got %q", "i3-WM", pkg)
 	}
 
+	// Verify that the pkg: alias is recognized (case-sensitively)
+	rewritten = rewrite(t, "/search?q=searchterm+pkg%3Ai3-WM")
+	querystr = rewritten.Query().Get("q")
+	if querystr != "searchterm" {
+		t.Fatalf("Expected search query %q, got %q", "searchterm", querystr)
+	}
+	pkg = rewritten.Query().Get("package")
+	if pkg != "i3-WM" {
+		t.Fatalf("Expected package %q, got %q", "i3-WM", pkg)
+	}
+
+	// Verify that the -pkg: (negative) alias is recognized (case-sensitively)
+	rewritten = rewrite(t, "/search?q=searchterm+-pkg%3Ai3-WM")
+	querystr = rewritten.Query().Get("q")
+	if querystr != "searchterm" {
+		t.Fatalf("Expected search query %q, got %q", "searchterm", querystr)
+	}
+	pkg = rewritten.Query().Get("npackage")
+	if pkg != "i3-WM" {
+		t.Fatalf("Expected package %q, got %q", "i3-WM", pkg)
+	}
+
 	// Verify that -file is translated to npath
 	rewritten = rewrite(t, "/search?q=searchterm+-file:foo")
 	querystr = rewritten.Query().Get("q")