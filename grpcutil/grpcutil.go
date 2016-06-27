081952aa85757ad532c36dc882ff5553a3efe3c1
Michael Stapelberg <michael@stapelberg.de>
2016-06-27 22:08:33 +0200

    fix TODO in comment

--- grpcutil/grpcutil.go
+++ grpcutil/grpcutil.go
@@ -26,8 +26,8 @@ var (
 // connections or otherHandler otherwise. Copied from cockroachdb.
 func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO(tamird): point to merged gRPC code rather than a PR.
-		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
+		// This is a partial recreation of gRPC's internal checks:
+		// https://github.com/grpc/grpc-go/blob/7834b974e55fbf85a5b01afb5821391c71084efd/transport/handler_server.go#L61
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
 		} else {
