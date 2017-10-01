Debian/dcs

commit c4e4211c0a3ba958e2a973ddecd38126d40e1f02

Add github.com/uber/jaeger tracing support

cmd/dcs-web/dcs-web.go
@@ -27,7 +27,11 @@ import (
 	"github.com/Debian/dcs/index"
 	dcsregexp "github.com/Debian/dcs/regexp"
 	_ "github.com/Debian/dcs/varz"
+	"github.com/opentracing-contrib/go-stdlib/nethttp"
+	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
 	_ "golang.org/x/net/trace"
 	"golang.org/x/net/websocket"
 )
@@ -48,6 +52,9 @@ var (
 		"Where to write access.log entries (in Apache Common Log Format). Disabled if empty.")
 	tlsCertPath = flag.String("tls_cert_path", "", "Path to a .pem file containing the TLS certificate.")
 	tlsKeyPath  = flag.String("tls_key_path", "", "Path to a .pem file containing the TLS private key.")
+	jaegerAgent = flag.String("jaeger_agent",
+		"localhost:5775",
+		"host:port of a github.com/uber/jaeger agent")
 
 	accessLog *os.File
 
@@ -95,6 +102,9 @@ func validateQuery(query string) error {
 }
 
 func EventsHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	span := opentracing.SpanFromContext(ctx)
+	span.SetOperationName("Events: " + r.URL.Path[len("/events/"):])
 	w.Header().Set("Content-Type", "text/event-stream")
 
 	// The additional ":" at the end is necessary so that we don’t need to
@@ -133,7 +143,7 @@ func EventsHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(h, q)
 	identifier := fmt.Sprintf("%x", h.Sum64())
 
-	cached, err := maybeStartQuery(identifier, src, q)
+	cached, err := maybeStartQuery(ctx, identifier, src, q)
 	if err != nil {
 		log.Printf("[%s] could not start query: %v\n", src, err)
 		http.Error(w, "Could not start query", http.StatusInternalServerError)
@@ -185,6 +195,7 @@ func EventsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func InstantServer(ws *websocket.Conn) {
+	ctx := ws.Request().Context()
 	// The additional ":" at the end is necessary so that we don’t need to
 	// distinguish between the two cases (X-Forwarded-For, without a port, and
 	// RemoteAddr, with a part) in the code below.
@@ -207,6 +218,10 @@ func InstantServer(ws *websocket.Conn) {
 			return
 		}
 		log.Printf("[%s] Received query %v\n", src, q)
+
+		span := opentracing.SpanFromContext(ctx)
+		span.SetOperationName("Websocket: " + q.Query)
+
 		if err := validateQuery("?" + q.Query); err != nil {
 			log.Printf("[%s] Query %q failed validation: %v\n", src, q.Query, err)
 			ws.Write([]byte(`{"Type":"error", "ErrorType":"invalidquery"}`))
@@ -220,7 +235,7 @@ func InstantServer(ws *websocket.Conn) {
 		io.WriteString(h, q.Query)
 		identifier := fmt.Sprintf("%x", h.Sum64())
 
-		cached, err := maybeStartQuery(identifier, src, q.Query)
+		cached, err := maybeStartQuery(ctx, identifier, src, q.Query)
 		if err != nil {
 			log.Printf("[%s] could not start query: %v\n", src, err)
 			ws.Write([]byte(`{"Type":"error", "ErrorType":"failed"}`))
@@ -331,7 +346,30 @@ func ResultsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	flag.Parse()
+
+	// Initialize the global tracer as early as possible:
+	// common.Init uses gRPC.
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  *jaegerAgent,
+		},
+	}
+	tracer, closer, err := cfg.New(
+		"dcs-web",
+		jaegercfg.Logger(jaeger.StdLogger),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	opentracing.SetGlobalTracer(tracer)
+	defer closer.Close()
 
 	common.Init(*tlsCertPath, *tlsKeyPath, *staticPath)
 
@@ -383,7 +421,6 @@ func main() {
 	})
 	http.HandleFunc("/favicon.ico", http.NotFound)
 	http.HandleFunc("/goroutinez", goroutinez.Goroutinez)
-	http.HandleFunc("/search", Search)
 	http.HandleFunc("/show", show.Show)
 	http.HandleFunc("/memprof", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("writing memprof")
@@ -402,7 +439,16 @@ func main() {
 	http.HandleFunc("/perpackage-results/", PerPackageResultsHandler)
 	http.HandleFunc("/queryz", QueryzHandler)
 	http.HandleFunc("/track", Track)
-	http.HandleFunc("/events/", EventsHandler)
+
+	traced := http.NewServeMux()
+	traced.HandleFunc("/search", Search)
+	traced.HandleFunc("/events/", EventsHandler)
+	traced.Handle("/instantws", websocket.Handler(InstantServer))
+	traceHandler := nethttp.Middleware(tracer, traced)
+	http.Handle("/events/", traceHandler)
+	http.Handle("/instantws", traceHandler)
+	http.Handle("/search", traceHandler)
+
 	// Used by the service worker.
 	http.HandleFunc("/placeholder.html", func(w http.ResponseWriter, r *http.Request) {
 		if err := common.Templates.ExecuteTemplate(w, "placeholder.html", map[string]interface{}{
@@ -416,7 +462,6 @@ func main() {
 		}
 	})
 
-	http.Handle("/instantws", websocket.Handler(InstantServer))
 	http.Handle("/metrics", prometheus.Handler())
 
 	if *listenAddressTLS != "" {

Gopkg.toml
@@ -19,3 +19,7 @@
 # [[override]]
 #  name = "github.com/x/y"
 #  version = "2.4.0"
+
+[[constraint]]
+  name = "github.com/uber/jaeger"
+  version = "0.7.0"

cmd/dcs-index-backend/index-backend.go
@@ -19,6 +19,8 @@ import (
 	_ "github.com/Debian/dcs/varz"
 	"github.com/google/codesearch/regexp"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -29,6 +31,9 @@ var (
 	cpuProfile    = flag.String("cpuprofile", "", "write cpu profile to this file")
 	tlsCertPath   = flag.String("tls_cert_path", "", "Path to a .pem file containing the TLS certificate.")
 	tlsKeyPath    = flag.String("tls_key_path", "", "Path to a .pem file containing the TLS private key.")
+	jaegerAgent   = flag.String("jaeger_agent",
+		"localhost:5775",
+		"host:port of a github.com/uber/jaeger agent")
 )
 
 type server struct {
@@ -121,11 +126,31 @@ func (s *server) ReplaceIndex(ctx context.Context, in *proto.ReplaceIndexRequest
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	flag.Parse()
 	if *indexPath == "" {
 		log.Fatal("You need to specify a non-empty -index_path")
 	}
 	fmt.Println("Debian Code Search index-backend")
+
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  *jaegerAgent,
+		},
+	}
+	closer, err := cfg.InitGlobalTracer(
+		"dcs-index-backend",
+		jaegercfg.Logger(jaeger.StdLogger),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closer.Close()
 
 	http.Handle("/metrics", prometheus.Handler())
 

cmd/dcs-source-backend/source-backend.go
@@ -22,7 +22,11 @@ import (
 	"github.com/Debian/dcs/ranking"
 	"github.com/Debian/dcs/regexp"
 	_ "github.com/Debian/dcs/varz"
+	opentracing "github.com/opentracing/opentracing-go"
+	olog "github.com/opentracing/opentracing-go/log"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -37,6 +41,9 @@ var (
 		"Path to the JSON containing ranking data")
 	tlsCertPath = flag.String("tls_cert_path", "", "Path to a .pem file containing the TLS certificate.")
 	tlsKeyPath  = flag.String("tls_key_path", "", "Path to a .pem file containing the TLS private key.")
+	jaegerAgent = flag.String("jaeger_agent",
+		"localhost:5775",
+		"host:port of a github.com/uber/jaeger agent")
 
 	indexBackend proto.IndexBackendClient
 )
@@ -186,14 +193,18 @@ func sendProgressUpdate(stream proto.SourceBackend_SearchServer, connMu *sync.Mu
 // Reads a single JSON request from the TCP connection, performs the search and
 // sends results back over the TCP connection as they appear.
 func (s *server) Search(in *proto.SearchRequest, stream proto.SourceBackend_SearchServer) error {
+	ctx := stream.Context()
 	connMu := new(sync.Mutex)
 	logprefix := fmt.Sprintf("[%q]", in.Query)
+	span := opentracing.SpanFromContext(ctx)
 
 	// Ask the local index backend for all the filenames.
-	resp, err := indexBackend.Files(context.Background(), &proto.FilesRequest{Query: in.Query})
+	resp, err := indexBackend.Files(ctx, &proto.FilesRequest{Query: in.Query})
 	if err != nil {
 		return fmt.Errorf("%s Error querying index backend for query %q: %v\n", logprefix, in.Query, err)
 	}
+
+	span.LogFields(olog.Int("files.possible", len(resp.Path)))
 
 	// Parse the (rewritten) URL to extract all ranking options/keywords.
 	rewritten, err := url.Parse(in.RewrittenUrl)
@@ -201,8 +212,10 @@ func (s *server) Search(in *proto.SearchRequest, stream proto.SourceBackend_Sear
 		return err
 	}
 	rankingopts := ranking.RankingOptsFromQuery(rewritten.Query())
+	span.LogFields(olog.String("rankingopts", fmt.Sprintf("%+v", rankingopts)))
 
 	// Rank all the paths.
+	rankspan, _ := opentracing.StartSpanFromContext(ctx, "Rank")
 	files := make(ranking.ResultPaths, 0, len(resp.Path))
 	for _, filename := range resp.Path {
 		result := ranking.ResultPath{Path: filename}
@@ -211,9 +224,14 @@ func (s *server) Search(in *proto.SearchRequest, stream proto.SourceBackend_Sear
 			files = append(files, result)
 		}
 	}
+	rankspan.Finish()
 
 	// Filter all files that should be excluded.
+	filterspan, _ := opentracing.StartSpanFromContext(ctx, "Filter")
 	files = filterByKeywords(rewritten, files)
+	filterspan.Finish()
+
+	span.LogFields(olog.Int("files.filtered", len(files)))
 
 	// While not strictly necessary, this will lead to better results being
 	// discovered (and returned!) earlier, so let’s spend a few cycles on
@@ -224,6 +242,8 @@ func (s *server) Search(in *proto.SearchRequest, stream proto.SourceBackend_Sear
 	if err != nil {
 		return fmt.Errorf("%s Could not compile regexp: %v\n", logprefix, err)
 	}
+
+	span.LogFields(olog.String("regexp", re.String()))
 
 	log.Printf("%s regexp = %q, %d possible files\n", logprefix, re, len(files))
 
@@ -376,7 +396,28 @@ func (s *server) Search(in *proto.SearchRequest, stream proto.SourceBackend_Sear
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	flag.Parse()
+
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  *jaegerAgent,
+		},
+	}
+	closer, err := cfg.InitGlobalTracer(
+		"dcs-source-backend",
+		jaegercfg.Logger(jaeger.StdLogger),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closer.Close()
+
 	rand.Seed(time.Now().UnixNano())
 	if !strings.HasSuffix(*unpackedPath, "/") {
 		*unpackedPath = *unpackedPath + "/"

cmd/dcs-web/querymanager.go
@@ -27,6 +27,7 @@ import (
 	pb "github.com/Debian/dcs/proto"
 	"github.com/Debian/dcs/stringpool"
 	"github.com/golang/protobuf/proto"
+	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/net/context"
 )
@@ -198,7 +199,7 @@ var (
 	stateMu sync.RWMutex
 )
 
-func queryBackend(queryid, src string, backend pb.SourceBackendClient, backendidx int, searchRequest *pb.SearchRequest) {
+func queryBackend(ctx context.Context, queryid, src string, backend pb.SourceBackendClient, backendidx int, searchRequest *pb.SearchRequest) {
 	// When exiting this function, check that all results were processed. If
 	// not, the backend query must have failed for some reason. Send a progress
 	// update to prevent the query from running forever.
@@ -227,7 +228,7 @@ func queryBackend(queryid, src string, backend pb.SourceBackendClient, backendid
 		})
 	}()
 
-	ctx, cancelfunc := context.WithCancel(context.Background())
+	ctx, cancelfunc := context.WithCancel(ctx)
 	stream, err := backend.Search(ctx, searchRequest)
 	if err != nil {
 		log.Printf("[%s] [src:%s] Search RPC failed: %v\n", queryid, src, err)
@@ -344,10 +345,18 @@ func startQuery(queryid string, querystate queryState) error {
 // maybeStartQuery starts a specified query if that query does not already
 // exist. Returns whether the query existed and any errors during query
 // creation.
-func maybeStartQuery(queryid, src, query string) (bool, error) {
+func maybeStartQuery(ctx context.Context, queryid, src, query string) (bool, error) {
 	if queryExists(queryid) {
 		return true, nil
 	}
+
+	// carry over the tracing span id to a background context: queries are
+	// executed independent of the client, so that when a link is posted
+	// somewhere popular, we don’t duplicate a bunch of work.
+	// TODO(golang.org/issues/19643): replace the code below once a “detach” API
+	// is available
+	span := opentracing.SpanFromContext(ctx)
+	ctx = opentracing.ContextWithSpan(context.Background(), span)
 
 	querystate := queryState{
 		started:        time.Now(),
@@ -401,7 +410,7 @@ func maybeStartQuery(queryid, src, query string) (bool, error) {
 		return true, nil
 	}
 	for idx, backend := range common.SourceBackendStubs {
-		go queryBackend(queryid, src, backend, idx, searchRequest)
+		go queryBackend(ctx, queryid, src, backend, idx, searchRequest)
 	}
 	return false, nil
 }

cmd/dcs-web/serverrendered.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/Debian/dcs/cmd/dcs-web/common"
 	dcsregexp "github.com/Debian/dcs/regexp"
+	opentracing "github.com/opentracing/opentracing-go"
 )
 
 // XXX: Using a dcsregexp.Match anonymous struct member doesn’t work,
@@ -206,6 +207,7 @@ func renderPerPackage(w http.ResponseWriter, r *http.Request, queryid string, pa
 // page= page number
 // perpkg= per-package grouping
 func Search(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, "Could not parse form data", http.StatusInternalServerError)
 		return
@@ -216,6 +218,9 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Empty query", http.StatusNotFound)
 		return
 	}
+
+	span := opentracing.SpanFromContext(ctx)
+	span.SetOperationName("Serverrendered: " + r.Form.Get("q"))
 
 	// We encode a URL that contains _only_ the q parameter.
 	q := url.Values{"q": []string{r.Form.Get("q")}}.Encode()
@@ -246,7 +251,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if _, err := maybeStartQuery(queryid, src, q); err != nil {
+	if _, err := maybeStartQuery(ctx, queryid, src, q); err != nil {
 		log.Printf("[%s] could not start query: %v\n", src, err)
 		http.Error(w, "Could not start query", http.StatusInternalServerError)
 		return

grpcutil/grpcutil.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/trace"
 	"google.golang.org/grpc"
@@ -62,7 +63,9 @@ func DialTLS(addr, certFile, keyFile string) (*grpc.ClientConn, error) {
 
 	return grpc.Dial(addr,
 		grpc.WithTransportCredentials(auth),
-		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(512*1024*1024)))
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(512*1024*1024)),
+		grpc.WithStreamInterceptor(grpc_opentracing.StreamClientInterceptor()),
+		grpc.WithUnaryInterceptor(grpc_opentracing.UnaryClientInterceptor()))
 }
 
 func ListenAndServeTLS(addr, certFile, keyFile string, register func(s *grpc.Server)) error {
@@ -79,7 +82,9 @@ func ListenAndServeTLS(addr, certFile, keyFile string, register func(s *grpc.Ser
 	s := grpc.NewServer(
 		grpc.Creds(auth),
 		grpc.MaxRecvMsgSize(512*1024*1024),
-		grpc.MaxSendMsgSize(512*1024*1024))
+		grpc.MaxSendMsgSize(512*1024*1024),
+		grpc.StreamInterceptor(grpc_opentracing.StreamServerInterceptor()),
+		grpc.UnaryInterceptor(grpc_opentracing.UnaryServerInterceptor()))
 
 	register(s)
 

commit b0aa9a563bcc4b20a539bc607a3f794d383b9510

Fix /instantws handler (Thanks jwilk)
fixes #84

This was broken with commit c4e4211c0a3ba958e2a973ddecd38126d40e1f02.

Stack trace:

Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: 2017/10/01 07:44:57 server.go:2848: http: panic serving [::1]:33090: interface conversion: *nethttp.statusCodeTracker is not http.Hijacker: missing method Hijack
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: goroutine 1287698 [running]:
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: net/http.(*conn).serve.func1(0xc4201726e0)
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]:         /home/michael/sdk/go1.9/src/net/http/server.go:1697 +0xd0
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: panic(0x91dbc0, 0xc4a0a5b080)
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]:         /home/michael/sdk/go1.9/src/runtime/panic.go:491 +0x283
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: github.com/Debian/dcs/vendor/golang.org/x/net/websocket.Server.serveWebSocket(0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, ...)
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]:         /home/michael/go/src/github.com/Debian/dcs/vendor/golang.org/x/net/websocket/server.go:74 +0x91
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: github.com/Debian/dcs/vendor/golang.org/x/net/websocket.Handler.ServeHTTP(0x9dfb90, 0xe96f40, 0xc421bd45a0, 0xc4201aa100)
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]:         /home/michael/go/src/github.com/Debian/dcs/vendor/golang.org/x/net/websocket/server.go:112 +0xb4
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: net/http.(*ServeMux).ServeHTTP(0xc420232cc0, 0xe96f40, 0xc421bd45a0, 0xc4201aa100)
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]:         /home/michael/sdk/go1.9/src/net/http/server.go:2254 +0x130
Oct 01 07:44:57 dcs-instant-web dcs-web[31174]: github.com/Debian/dcs/vendor/github.com/opentracing-contrib/go-stdlib/nethttp.Middleware.func2(0xe97b40, 0xc420323180, 0xc421610800)

cmd/dcs-web/dcs-web.go
@@ -219,8 +219,8 @@ func InstantServer(ws *websocket.Conn) {
 		}
 		log.Printf("[%s] Received query %v\n", src, q)
 
-		span := opentracing.SpanFromContext(ctx)
-		span.SetOperationName("Websocket: " + q.Query)
+		// span := opentracing.SpanFromContext(ctx)
+		// span.SetOperationName("Websocket: " + q.Query)
 
 		if err := validateQuery("?" + q.Query); err != nil {
 			log.Printf("[%s] Query %q failed validation: %v\n", src, q.Query, err)
@@ -446,7 +446,10 @@ func main() {
 	traced.Handle("/instantws", websocket.Handler(InstantServer))
 	traceHandler := nethttp.Middleware(tracer, traced)
 	http.Handle("/events/", traceHandler)
-	http.Handle("/instantws", traceHandler)
+	// TODO: find a way to trace /instantws calls — re-implement the
+	// http.Hijacker interface in nethttp.Middleware?
+	// http.Handle("/instantws", traceHandler)
+	http.Handle("/instantws", websocket.Handler(InstantServer))
 	http.Handle("/search", traceHandler)
 
 	// Used by the service worker.