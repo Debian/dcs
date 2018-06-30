f56f69b4b2aa765fed3c416cf1623f2ce48a2369
Michael Stapelberg <michael@stapelberg.de>
2018-06-30 08:44:17 +0200

    types: remove unused OffsetCtrl

--- internal/index/types.go
+++ internal/index/types.go
@@ -14,30 +14,29 @@ type Trigram uint32
 type MetaEntry struct {
 	Trigram    Trigram
 	Entries    uint32 // number of entries (excluding padding)
-	OffsetCtrl int64  // control bytes offset within the corresponding .ctrl file
-	OffsetData int64  // streaming vbyte deltas offset within the corresponding .data file
+	OffsetData int64  // delta offset within the corresponding .data or .turbopfor file
 }
 
-// TODO(performance): check if the compiler turns binary.Size into a constant
-const metaEntrySize = 24 // (encoding/binary).Size(MetaEntry)
+const (
+	// metaEntrySize is (encoding/binary).Size(&MetaEntry{}), which Go 1.11 does
+	// not turn into a compile-time constant yet.
+	metaEntrySize = 16
+)
 
-var meBuf [24]byte // TODO: make concurrency safe
+var meBuf [metaEntrySize]byte // TODO: make concurrency safe
 func writeMetaEntry(w io.Writer, me *MetaEntry) error {
 	n := 0
 	binary.LittleEndian.PutUint32(meBuf[n:], uint32(me.Trigram))
 	n += 4
 	binary.LittleEndian.PutUint32(meBuf[n:], (me.Entries))
 	n += 4
-	binary.LittleEndian.PutUint64(meBuf[n:], uint64(me.OffsetCtrl))
-	n += 8
 	binary.LittleEndian.PutUint64(meBuf[n:], uint64(me.OffsetData))
-	n += 8
 	_, err := w.Write(meBuf[:])
 	return err
 }
 
 func readMetaEntry(r io.Reader, me *MetaEntry) error {
-	var meBuf [24]byte
+	var meBuf [metaEntrySize]byte
 	if _, err := io.ReadFull(r, meBuf[:]); err != nil {
 		return err
 	}
@@ -47,8 +46,6 @@ func readMetaEntry(r io.Reader, me *MetaEntry) error {
 	n += 4
 	me.Entries = binary.LittleEndian.Uint32(meBuf[n:])
 	n += 4
-	me.OffsetCtrl = int64(binary.LittleEndian.Uint64(meBuf[n:]))
-	n += 8
 	me.OffsetData = int64(binary.LittleEndian.Uint64(meBuf[n:]))
 
 	return nil
@@ -60,7 +57,5 @@ func (me *MetaEntry) Unmarshal(b []byte) {
 	n += 4
 	me.Entries = binary.LittleEndian.Uint32(b[n:])
 	n += 4
-	me.OffsetCtrl = int64(binary.LittleEndian.Uint64(b[n:]))
-	n += 8
 	me.OffsetData = int64(binary.LittleEndian.Uint64(b[n:]))
 }
