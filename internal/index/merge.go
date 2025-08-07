d80b8f1d20916285af563e7b61ab1f5266b7eff2
Michael Stapelberg <michael@stapelberg.de>
2025-08-07 19:28:50 +0200

    internal/index: refactor writing into separate functions
    This makes it a little easier to keep track of data flow.

--- internal/index/merge.go
+++ internal/index/merge.go
@@ -171,238 +171,255 @@ func ConcatN(destdir string, srcdirs []string) error {
 	}
 	sort.Slice(trigrams, func(i, j int) bool { return trigrams[i] < trigrams[j] })
 
-	{
-		log.Printf("writing merged docids")
-		dw, err := newPForWriter(destdir, "docid")
-		if err != nil {
-			return err
-		}
-
-		fDocidMeta, err := os.Create(filepath.Join(destdir, "posting.docid.meta"))
-		if err != nil {
-			return err
-		}
-		defer fDocidMeta.Close()
-		bufwDocidMeta := bufio.NewWriter(fDocidMeta)
-
-		meBuf := make([]byte, metaEntrySize)
-		dr := NewDeltaReader()
-		var meta MetaEntry
-		for _, t := range trigrams {
-			if debug {
-				if t != debugTrigram {
-					continue
-				}
-			}
-
-			//for _, t := range []trigram{trigram(6650227), trigram(7959906)} {
-			//ctrl, data := dw.Offsets()
-			me := MetaEntry{
-				Trigram: t,
-				//OffsetCtrl: ctrl,
-				//OffsetEnc:  data,
-				OffsetData: dw.Offset(),
-			}
-			var last uint32
-			for _, idxid := range idxDocid[t] {
-				idx := idxMetaDocid[idxid]
-				if err := idx.rd.metaEntry1(&meta, t); err != nil {
-					if err == errNotFound {
-						continue
-					}
-					return err
-				}
-				me.Entries += meta.Entries
-				dr.Reset(&meta, idx.rd.data.Data)
-				docids := dr.Read() // returns non-nil at least once
-				// Bump the first docid: it needs to be mapped from the old
-				// docid range [0, n) to the new docid range [base, base+n).
-				//
-				// Since we are building a single docid list for this trigram,
-				// the new value needs to be a delta, hence, subtract last.
-				docids[0] += (idx.docidBase - last)
-				for docids != nil {
-					for _, d := range docids {
-						if err := dw.PutUint32(d); err != nil {
-							return err
-						}
-						last += d
-					}
-					docids = dr.Read()
-				}
-			}
-			if err := dw.Flush(); err != nil {
-				return err
-			}
-			me.Marshal(meBuf)
-			if _, err := bufwDocidMeta.Write(meBuf); err != nil {
-				//if err := binary.Write(bufwDocidMeta, binary.LittleEndian, &me); err != nil {
-				return err
-			}
-		}
-
-		if err := bufwDocidMeta.Flush(); err != nil {
-			return err
-		}
-
-		if err := fDocidMeta.Close(); err != nil {
-			return err
-		}
-
-		if err := dw.Close(); err != nil {
-			return err
-		}
+	if err := writeDocids(destdir, trigrams, idxDocid, idxMetaDocid); err != nil {
+		return nil
 	}
 
-	{
-		log.Printf("writing merged posrel")
-		fmetaf, err := os.Create(filepath.Join(destdir, "posting.posrel.meta"))
-		if err != nil {
-			return err
-		}
-		defer fmetaf.Close()
-		bufwmeta := bufio.NewWriter(fmetaf)
+	if err := writePosrel(destdir, trigrams, idxDocid, idxMetaPos, idxMetaPosrel); err != nil {
+		return err
+	}
 
-		fposrel, err := os.Create(filepath.Join(destdir, "posting.posrel.data"))
-		if err != nil {
-			return err
-		}
-		defer fposrel.Close()
-		cw := newCountingWriter(fposrel)
-		pw := newPosrelWriter(&cw)
-		var fmeta, pmeta MetaEntry
-		for _, t := range trigrams {
-			if debug {
-				if t != debugTrigram {
-					continue
-				}
-			}
-			if t == 2105376 { // TODO: document: "   "?
+	if err := writePos(destdir, trigrams, idxDocid, idxMetaPos); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeDocids(destdir string, trigrams []Trigram, idxDocid map[Trigram][]uint32, idxMetaDocid []indexMeta) error {
+	log.Printf("writing merged docids")
+	dw, err := newPForWriter(destdir, "docid")
+	if err != nil {
+		return err
+	}
+
+	fDocidMeta, err := os.Create(filepath.Join(destdir, "posting.docid.meta"))
+	if err != nil {
+		return err
+	}
+	defer fDocidMeta.Close()
+	bufwDocidMeta := bufio.NewWriter(fDocidMeta)
+
+	meBuf := make([]byte, metaEntrySize)
+	dr := NewDeltaReader()
+	var meta MetaEntry
+	for _, t := range trigrams {
+		if debug {
+			if t != debugTrigram {
 				continue
 			}
-
-			me := MetaEntry{
-				Trigram:    t,
-				OffsetData: int64(cw.offset),
-			}
-			if err := binary.Write(bufwmeta, binary.LittleEndian, &me); err != nil {
-				return err
-			}
-			for _, idxid := range idxDocid[t] {
-				// TODO: refactor all metaEntry1 to use ,ok idiom, they only ever return errNotFound
-				if err := idxMetaPos[idxid].rd.metaEntry1(&fmeta, t); err != nil {
-					if err == errNotFound {
-						continue
-					}
-					return err
-				}
-
-				if err := idxMetaPosrel[idxid].rd.metaEntry1(&pmeta, t); err != nil {
-					if err == errNotFound {
-						continue
-					}
-					return err
-				}
-				b := idxMetaPosrel[idxid].rd.data.Data[pmeta.OffsetData:]
-				if err := pw.Write(b, int(fmeta.Entries)); err != nil {
-					return err
-				}
-
-			}
-			if err := pw.Flush(); err != nil {
-				return err
-			}
-		}
-		if err := bufwmeta.Flush(); err != nil {
-			return err
-		}
-		if err := fmetaf.Close(); err != nil {
-			return err
-		}
-		if err := cw.Close(); err != nil {
-			return err
-		}
-	}
-
-	{
-		log.Printf("writing merged pos")
-		dw, err := newPForWriter(destdir, "pos")
-		if err != nil {
-			return err
 		}
 
-		fDocidMeta, err := os.Create(filepath.Join(destdir, "posting.pos.meta"))
-		if err != nil {
-			return err
-		}
-		defer fDocidMeta.Close()
-		bufwDocidMeta := bufio.NewWriter(fDocidMeta)
-
-		meBuf := make([]byte, metaEntrySize)
-		dr := NewDeltaReader()
-		var meta MetaEntry
 		//for _, t := range []trigram{trigram(6650227), trigram(7959906)} {
-		for _, t := range trigrams {
-			if debug {
-				if t != debugTrigram {
+		//ctrl, data := dw.Offsets()
+		me := MetaEntry{
+			Trigram: t,
+			//OffsetCtrl: ctrl,
+			//OffsetEnc:  data,
+			OffsetData: dw.Offset(),
+		}
+		var last uint32
+		for _, idxid := range idxDocid[t] {
+			idx := idxMetaDocid[idxid]
+			if err := idx.rd.metaEntry1(&meta, t); err != nil {
+				if err == errNotFound {
 					continue
 				}
+				return err
 			}
+			me.Entries += meta.Entries
+			dr.Reset(&meta, idx.rd.data.Data)
+			docids := dr.Read() // returns non-nil at least once
+			// Bump the first docid: it needs to be mapped from the old
+			// docid range [0, n) to the new docid range [base, base+n).
+			//
+			// Since we are building a single docid list for this trigram,
+			// the new value needs to be a delta, hence, subtract last.
+			docids[0] += (idx.docidBase - last)
+			for docids != nil {
+				for _, d := range docids {
+					if err := dw.PutUint32(d); err != nil {
+						return err
+					}
+					last += d
+				}
+				docids = dr.Read()
+			}
+		}
+		if err := dw.Flush(); err != nil {
+			return err
+		}
+		me.Marshal(meBuf)
+		if _, err := bufwDocidMeta.Write(meBuf); err != nil {
+			//if err := binary.Write(bufwDocidMeta, binary.LittleEndian, &me); err != nil {
+			return err
+		}
+	}
 
-			if t == 2105376 { // TODO: document: "   "?
+	if err := bufwDocidMeta.Flush(); err != nil {
+		return err
+	}
+
+	if err := fDocidMeta.Close(); err != nil {
+		return err
+	}
+
+	if err := dw.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writePosrel(destdir string, trigrams []Trigram, idxDocid map[Trigram][]uint32, idxMetaPos []indexMeta, idxMetaPosrel []posrelMeta) error {
+	log.Printf("writing merged posrel")
+	fmetaf, err := os.Create(filepath.Join(destdir, "posting.posrel.meta"))
+	if err != nil {
+		return err
+	}
+	defer fmetaf.Close()
+	bufwmeta := bufio.NewWriter(fmetaf)
+
+	fposrel, err := os.Create(filepath.Join(destdir, "posting.posrel.data"))
+	if err != nil {
+		return err
+	}
+	defer fposrel.Close()
+	cw := newCountingWriter(fposrel)
+	pw := newPosrelWriter(&cw)
+	var fmeta, pmeta MetaEntry
+	for _, t := range trigrams {
+		if debug {
+			if t != debugTrigram {
 				continue
 			}
+		}
+		if t == 2105376 { // TODO: document: "   "?
+			continue
+		}
 
-			//ctrl, data := dw.Offsets()
-			me := MetaEntry{
-				Trigram: t,
-				// OffsetCtrl: ctrl,
-				// OffsetEnc:  data,
-				OffsetData: dw.Offset(),
-			}
-
-			for _, idxid := range idxDocid[t] {
-				idx := idxMetaPos[idxid]
-				if err := idx.rd.metaEntry1(&meta, t); err != nil {
-					if err == errNotFound {
-						continue
-					}
-					return err
+		me := MetaEntry{
+			Trigram:    t,
+			OffsetData: int64(cw.offset),
+		}
+		if err := binary.Write(bufwmeta, binary.LittleEndian, &me); err != nil {
+			return err
+		}
+		for _, idxid := range idxDocid[t] {
+			// TODO: refactor all metaEntry1 to use ,ok idiom, they only ever return errNotFound
+			if err := idxMetaPos[idxid].rd.metaEntry1(&fmeta, t); err != nil {
+				if err == errNotFound {
+					continue
 				}
-				me.Entries += meta.Entries
-				dr.Reset(&meta, idx.rd.data.Data)
-
-				for docids := dr.Read(); docids != nil; docids = dr.Read() {
-					for _, d := range docids {
-						if err := dw.PutUint32(d); err != nil {
-							return err
-						}
-					}
-				}
-			}
-
-			if err := dw.Flush(); err != nil {
 				return err
 			}
 
-			me.Marshal(meBuf)
-			if _, err := bufwDocidMeta.Write(meBuf); err != nil {
-				//if err := binary.Write(bufwDocidMeta, binary.LittleEndian, &me); err != nil {
+			if err := idxMetaPosrel[idxid].rd.metaEntry1(&pmeta, t); err != nil {
+				if err == errNotFound {
+					continue
+				}
 				return err
+			}
+			b := idxMetaPosrel[idxid].rd.data.Data[pmeta.OffsetData:]
+			if err := pw.Write(b, int(fmeta.Entries)); err != nil {
+				return err
+			}
+
+		}
+		if err := pw.Flush(); err != nil {
+			return err
+		}
+	}
+	if err := bufwmeta.Flush(); err != nil {
+		return err
+	}
+	if err := fmetaf.Close(); err != nil {
+		return err
+	}
+	if err := cw.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writePos(destdir string, trigrams []Trigram, idxDocid map[Trigram][]uint32, idxMetaPos []indexMeta) error {
+	log.Printf("writing merged pos")
+	dw, err := newPForWriter(destdir, "pos")
+	if err != nil {
+		return err
+	}
+
+	fDocidMeta, err := os.Create(filepath.Join(destdir, "posting.pos.meta"))
+	if err != nil {
+		return err
+	}
+	defer fDocidMeta.Close()
+	bufwDocidMeta := bufio.NewWriter(fDocidMeta)
+
+	meBuf := make([]byte, metaEntrySize)
+	dr := NewDeltaReader()
+	var meta MetaEntry
+	//for _, t := range []trigram{trigram(6650227), trigram(7959906)} {
+	for _, t := range trigrams {
+		if debug {
+			if t != debugTrigram {
+				continue
 			}
 		}
 
-		if err := bufwDocidMeta.Flush(); err != nil {
+		if t == 2105376 { // TODO: document: "   "?
+			continue
+		}
+
+		//ctrl, data := dw.Offsets()
+		me := MetaEntry{
+			Trigram: t,
+			// OffsetCtrl: ctrl,
+			// OffsetEnc:  data,
+			OffsetData: dw.Offset(),
+		}
+
+		for _, idxid := range idxDocid[t] {
+			idx := idxMetaPos[idxid]
+			if err := idx.rd.metaEntry1(&meta, t); err != nil {
+				if err == errNotFound {
+					continue
+				}
+				return err
+			}
+			me.Entries += meta.Entries
+			dr.Reset(&meta, idx.rd.data.Data)
+
+			for docids := dr.Read(); docids != nil; docids = dr.Read() {
+				for _, d := range docids {
+					if err := dw.PutUint32(d); err != nil {
+						return err
+					}
+				}
+			}
+		}
+
+		if err := dw.Flush(); err != nil {
 			return err
 		}
 
-		if err := fDocidMeta.Close(); err != nil {
+		me.Marshal(meBuf)
+		if _, err := bufwDocidMeta.Write(meBuf); err != nil {
+			//if err := binary.Write(bufwDocidMeta, binary.LittleEndian, &me); err != nil {
 			return err
 		}
+	}
 
-		if err := dw.Close(); err != nil {
-			return err
-		}
+	if err := bufwDocidMeta.Flush(); err != nil {
+		return err
+	}
+
+	if err := fDocidMeta.Close(); err != nil {
+		return err
+	}
+
+	if err := dw.Close(); err != nil {
+		return err
 	}
 
 	return nil
